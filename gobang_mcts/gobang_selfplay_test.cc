40753c6335465f4e21ca0b6fd9212457c643a1d2
CWHer <cwher@outlook.com>
2023-03-30 18:06:59 +0000

    style: rename file

--- gobang_mcts/gobang_selfplay_test.cc
+++ gobang_mcts/gobang_selfplay_test.cc
@@ -1,12 +1,12 @@
-#include "envpool/gobang_mcts/gobang_mcts.hpp"
+#include "envpool/gobang_mcts/gobang_selfplay.hpp"
 
 #include <numeric>
 #include <gtest/gtest.h>
 
-TEST(SelfPlay, Small)
+TEST(GobangSelfPlayTest, Small)
 {
-    int num_search = 1000;
-    SelfPlayGobang game(3, 3, 1.0f, num_search);
+    int num_search = 20000;
+    GobangSelfPlay game(3, 3, 1.0f, num_search, 1e-5);
     int player = 0;
     int step_count = 0, display_count = 0;
     game.reset();
@@ -14,7 +14,7 @@ TEST(SelfPlay, Small)
     EXPECT_FALSE(done);
     while (!done)
     {
-        std::vector<float> prior_probs(5 * 5, .1f);
+        std::vector<float> prior_probs(3 * 3, .1f);
         float value = 0.0;
         done = game.step(prior_probs, value);
 
@@ -32,7 +32,7 @@ TEST(SelfPlay, Small)
     }
 
     EXPECT_EQ(display_count, 3 * 3);
-    std::cout << "step: " << step_count << std::endl;
+    std::cout << "Step: " << step_count << std::endl;
     EXPECT_LT(step_count, num_search * 3 * 3);
     auto winner = game.getWinner();
     EXPECT_TRUE(winner == -1); // when num_search is large enough
