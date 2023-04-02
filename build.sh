c2b8967007875fe6c3711ecf22e5564dd7274f87
CWHer <cwher@outlook.com>
2023-04-02 04:43:26 +0000

    chore: add build script

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# create conda environment
+eval "$(conda shell.bash hook)"
+conda activate envpool-dev
+if [ $? -ne 0 ]; then
+    conda create -n envpool-dev python=3.9
+    conda activate envpool-dev
+fi
+
+ENVPOOL_NAME=minimal-envpool
+ENV_NAME=gobang_mcts
+
+# clone envpool
+rm -rf ${ENVPOOL_NAME}
+git clone git@github.com:CWHer/minimal-envpool.git
+ln -s ${PWD}/${ENV_NAME} ${ENVPOOL_NAME}/envpool/${ENV_NAME}
+cd ${ENVPOOL_NAME}
+
+# apply patches
+git apply ../patches/envpool.patch
+
+# build all
+make bazel-clean
+make bazel-test
+# generate .whl file
+make bazel-release
+# install .whl
+pip install dist/* --force-reinstall
+
+cd ..
+python -m unittest gobang_envpool_test.py
+if [ $? -ne 0 ]; then
+    echo "Test failed!"
+    exit 1
+fi
+
+conda deactivate
