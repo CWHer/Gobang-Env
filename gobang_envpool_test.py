43b595729dbfa87c45f2abe221fee92588fc96ae
CWHer <cwher@outlook.com>
2023-04-06 15:10:47 +0000

    fix: fix bug in action shape & add more state test

--- gobang_envpool_test.py
+++ gobang_envpool_test.py
@@ -28,22 +28,25 @@ class GobangTest(unittest.TestCase):
         collect_this_state = [False for _ in range(num_envs)]
         player_step_count = [0 for _ in range(num_envs)]
         winner = [-1 for _ in range(num_envs)]
-        action = [0 for _ in range(num_envs)]
+        selected_action = np.zeros((num_envs, ), dtype=np.int32)
 
         env.async_reset()
         with tqdm.tqdm(total=num_envs) as pbar:
             while not all(done):
                 obs, reward, terminated, truncated, info = env.recv()
                 self.assertTrue(np.logical_not(np.all(truncated)))
+                self.assertTrue(
+                    np.all(info["env_id"][:-1] <= info["env_id"][1:]))
                 is_player_done = info["is_player_done"]
                 for i in range(num_envs):
                     if not done[i] and is_player_done[i]:
                         collect_this_state[i] = True
                         player_step_count[i] += 1
                         probs[i].append(obs.mcts_result[i])
+                    if is_player_done[i]:
                         mcts_result = obs.mcts_result[i]
                         mcts_result[mcts_result < 0] = 0
-                        action[i] = np.random.choice(
+                        selected_action[i] = np.random.choice(
                             np.arange(15 * 15), p=mcts_result / np.sum(mcts_result))
 
                 for i in range(num_envs):
@@ -68,7 +71,7 @@ class GobangTest(unittest.TestCase):
                 actions = {
                     "prior_probs": 0.1 * np.ones((num_envs, 15 * 15), dtype=np.float32),
                     "value": 0.1 * np.ones((num_envs, ), dtype=np.float32),
-                    "action": np.array(action, dtype=np.int32)
+                    "selected_action": selected_action,
                 }
                 env.send(actions, env_id)
 
@@ -80,12 +83,14 @@ class GobangTest(unittest.TestCase):
             )
             self.assertEqual(len(state[i]), len(probs[i]))
             self.assertEqual(len(state[i]), player_step_count[i])
-            for j, s in enumerate(state[i]):
+            self.assertTrue(np.all(state[i][0] == 0))
+            for j, s in enumerate(state[i][1:], start=1):
                 self.assertTrue(np.all(s[-1] == j % 2),
                                 f"State {j} of env {i} is wrong")
                 player0_step = np.sum(s[0])
                 player1_step = np.sum(s[num_player_planes])
                 self.assertTrue(player0_step + player1_step == j)
+                self.assertLessEqual(np.abs(player0_step - player1_step), 1)
                 for k in range(1, num_player_planes):
                     self.assertTrue(
                         np.sum(s[k]) == max(0, player0_step - k),
@@ -95,6 +100,17 @@ class GobangTest(unittest.TestCase):
                         np.sum(s[k + num_player_planes]
                                ) == max(0, player1_step - k),
                         f"Wrong at [{i}, {j}, {k}]"
+                    )
+
+                res = state[i][j] - state[i][j - 1]
+                for k in range(num_player_planes):
+                    self.assertTrue(
+                        np.all(res[k] >= 0)
+                        and np.sum(res[k]) <= 1
+                    )
+                    self.assertTrue(
+                        np.all(res[k + num_player_planes] >= 0)
+                        and np.sum(res[k + num_player_planes]) <= 1
                     )
 
     def testAsync(self):
@@ -114,7 +130,7 @@ class GobangTest(unittest.TestCase):
         collect_this_state = [False for _ in range(num_envs)]
         player_step_count = [0 for _ in range(num_envs)]
         winner = [-1 for _ in range(num_envs)]
-        action = [0 for _ in range(num_envs)]
+        selected_action = np.zeros((num_envs, ), dtype=np.int32)
 
         env.async_reset()
         with tqdm.tqdm(total=num_envs) as pbar:
@@ -127,9 +143,10 @@ class GobangTest(unittest.TestCase):
                         collect_this_state[index] = True
                         player_step_count[index] += 1
                         probs[index].append(obs.mcts_result[i])
+                    if is_player_done[i]:
                         mcts_result = obs.mcts_result[i]
                         mcts_result[mcts_result < 0] = 0
-                        action[i] = np.random.choice(
+                        selected_action[index] = np.random.choice(
                             np.arange(15 * 15), p=mcts_result / np.sum(mcts_result))
 
                 for i, index in enumerate(info["env_id"]):
@@ -152,9 +169,9 @@ class GobangTest(unittest.TestCase):
 
                 env_id = info["env_id"]
                 actions = {
-                    "prior_probs": 0.1 * np.ones((num_envs, 15 * 15), dtype=np.float32),
-                    "value": 0.1 * np.ones((num_envs, ), dtype=np.float32),
-                    "action": np.array(action, dtype=np.int32)
+                    "prior_probs": 0.1 * np.ones((batch_size, 15 * 15), dtype=np.float32),
+                    "value": 0.1 * np.ones((batch_size, ), dtype=np.float32),
+                    "selected_action": selected_action[env_id]
                 }
                 env.send(actions, env_id)
 
@@ -166,12 +183,15 @@ class GobangTest(unittest.TestCase):
             )
             self.assertEqual(len(state[i]), len(probs[i]))
             self.assertEqual(len(state[i]), player_step_count[i])
-            for j, s in enumerate(state[i]):
+            self.assertTrue(np.all(state[i][0] == 0))
+            for j, s in enumerate(state[i][1:], start=1):
                 self.assertTrue(np.all(s[-1] == j % 2),
                                 f"State {j} of env {i} is wrong")
                 player0_step = np.sum(s[0])
                 player1_step = np.sum(s[num_player_planes])
-                self.assertTrue(player0_step + player1_step == j)
+                self.assertEqual(player0_step + player1_step, j,
+                                 f"Wrong at [{i}, {j}]")
+                self.assertLessEqual(np.abs(player0_step - player1_step), 1)
                 for k in range(1, num_player_planes):
                     self.assertTrue(
                         np.sum(s[k]) == max(0, player0_step - k),
@@ -183,25 +203,38 @@ class GobangTest(unittest.TestCase):
                         f"Wrong at [{i}, {j}, {k}]"
                     )
 
+                res = state[i][j] - state[i][j - 1]
+                for k in range(num_player_planes):
+                    self.assertTrue(
+                        np.all(res[k] >= 0)
+                        and np.sum(res[k]) <= 1
+                    )
+                    self.assertTrue(
+                        np.all(res[k + num_player_planes] >= 0)
+                        and np.sum(res[k + num_player_planes]) <= 1
+                    )
+
     def testThroughput(self):
         num_envs = 400
         batch_size = 128
-        num_threads = 4
+        num_threads = 10
+        num_search = 10
         env = envpool.make_gym(
             "GobangSelfPlay", num_envs=num_envs,
-            batch_size=batch_size, num_threads=num_threads
+            batch_size=batch_size, num_threads=num_threads,
+            num_search=num_search,
         )
 
         actions = {
-            "prior_probs": 0.1 * np.ones((num_envs, 15 * 15), dtype=np.float32),
-            "value": 0.1 * np.ones((num_envs, ), dtype=np.float32),
+            "prior_probs": 0.1 * np.ones((batch_size, 15 * 15), dtype=np.float32),
+            "value": 0.1 * np.ones((batch_size, ), dtype=np.float32),
         }
         n_steps = 1000
         env.async_reset()
         start = time.time()
         for _ in tqdm.trange(n_steps):
             obs, reward, terminated, truncated, info = env.recv()
-            actions["action"] = np.argmax(
+            actions["selected_action"] = np.argmax(
                 obs["mcts_result"], axis=1).astype(np.int32)
             env.send(actions, info["env_id"])
         duration = time.time() - start
